e3e1709a1d1fea8e0c426b2b20da0362889da733
LuisgMarin <marnluis189@gmail.com>
2023-09-18 10:05:38 -0500

    fix: flujo de autenticacion y lista candidatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,74 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { APP_ROUTING } from './app.routes';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { HttpClientModule } from '@angular/common/http';
+import { CandidatosComponent } from './candidatos/candidatos.component';
+import { CandidatosService } from './candidatos/candidato.service';
+import { UserRoleService } from './services/user-role.service';
+import { AuthenticationService } from './services/authentication.service';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+const routes: Routes=[
+  {path:'inicio-sesion',component:InicioSesionComponent},
+  {path:'home',component:HomeComponent},
+  {path:'candidatos',component:CandidatosComponent},
+  {path:'', pathMatch: 'full', redirectTo: 'home'},
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     InicioSesionComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    CandidatosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
     FormsModule, ReactiveFormsModule,
-    APP_ROUTING,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
   ],
-  providers: [],
+  providers: [CandidatosService, UserRoleService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
